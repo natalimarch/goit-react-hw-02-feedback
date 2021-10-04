3e70ece3d8592dd701e51f30b3c640547b8b6bb3
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-10-04 17:22:44 +0300

    task1 fix

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,9 +4,7 @@ import Section from "../Section/Section";
 import Statistics from "../Statistics/Statistics";
 import FeedbackOptions from "../FeedbackOptions/FeedbackOptions";
 
-
 class Feedback extends Component {
-  
   state = {
     good: 0,
     neutral: 0,
@@ -17,42 +15,46 @@ class Feedback extends Component {
     e.preventDefault();
     console.log(e.currentTarget.name);
     const currentState = e.currentTarget.name;
-    this.setState(prevState => ({
-      [currentState]: prevState.[currentState] + 1,
+    this.setState((prevState) => ({
+      [currentState]: prevState[currentState] + 1,
     }));
     this.countTotalFeedback();
   };
 
   countTotalFeedback = () => {
-    const {good, neutral, bad} = this.state;
+    const { good, neutral, bad } = this.state;
     const total = good + neutral + bad;
     return total;
   };
 
   countPositiveFeedbackPercentage = () => {
-    const {good} = this.state;
-    const {countTotalFeedback} = this;
+    const { good } = this.state;
+    const { countTotalFeedback } = this;
     const total = countTotalFeedback();
     const positive = Math.round((good / total) * 100);
-   return positive;
-  }
-  
+    return positive;
+  };
+
   render() {
-    const { countFeedback, countTotalFeedback, countPositiveFeedbackPercentage } = this;
-    const { good, neutral, bad} = this.state;
-    const onLeaveFeedback = {countFeedback};
+    const {
+      countFeedback,
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+    } = this;
+    const { good, neutral, bad } = this.state;
+    const onLeaveFeedback = { countFeedback };
     const names = ["good", "neutral", "bad"];
     return (
       <div className={styles.FeedbackForm}>
-        < Section title="Please leave feedback"/>
-        < FeedbackOptions options={names} onLeaveFeedback={onLeaveFeedback}/>
-        < Section title="Statistics"/>
-        < Statistics 
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={countTotalFeedback()}
-        positive={countPositiveFeedbackPercentage()}
+        <Section title="Please leave feedback" />
+        <FeedbackOptions options={names} onLeaveFeedback={onLeaveFeedback} />
+        <Section title="Statistics" />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positive={countPositiveFeedbackPercentage()}
         />
       </div>
     );
@@ -60,4 +62,3 @@ class Feedback extends Component {
 }
 
 export default Feedback;
-
